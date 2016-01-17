8132d4fc10bcbc644bcde58f20ca590a13599c58
clock <1084991652clock@gmail.com>
2016-01-17 23:21:52 +0800

    添加字符串处理实用工具类，目前增加字符串进行MD5转换的功能

---
+++ src/main/java/com/clock/utils/text/StringUtils.java
@@ -0,0 +1,41 @@
+package com.clock.utils.text;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * 字符串通用处理类
+ * Created by Clock on 2016/1/17.
+ */
+public class StringUtils {
+
+    /**
+     * 将字符串进行md5转换
+     *
+     * @param str
+     * @return
+     */
+    public static String md5(String str) {
+        String cacheKey;
+        try {
+            final MessageDigest mDigest = MessageDigest.getInstance("MD5");
+            mDigest.update(str.getBytes());
+            cacheKey = bytesToHexString(mDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            cacheKey = String.valueOf(str.hashCode());
+        }
+        return cacheKey;
+    }
+
+    private static String bytesToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(0xFF & bytes[i]);
+            if (hex.length() == 1) {
+                sb.append('0');
+            }
+            sb.append(hex);
+        }
+        return sb.toString();
+    }
+}
